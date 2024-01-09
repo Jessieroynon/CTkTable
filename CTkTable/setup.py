7d631465edd0ff0bd111502e67913d425d1ef36e
Jessie <115507036+Jessieroynon@users.noreply.github.com>
2024-01-09 13:09:32 -0700

    Create setup.py

---
+++ CTkTable/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CTkTable',
+    version='0.1',  # Version of your library
+    packages=find_packages(),
+    # Add additional fields as necessary
+)
